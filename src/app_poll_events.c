23da800aea6ea4fd3dd0d42f11dd2650b2ed82ae
YehorDzhurynskyi <y.a.dzhurynskyi@gmail.com>
2020-02-29 17:58:32 +0200

    event processing loop refactored

--- src/app_poll_events.c
+++ src/app_poll_events.c
@@ -12,69 +12,30 @@
 
 #include "app.h"
 
-static void process_event(SDL_Event *event)
+static void process_event(SDL_Event* event, t_app* app, t_scene_interactor* interactor)
 {
-    switch (event->type)
+    t_actor* a;
+
+    a = interactor->actor_selected;
+    if (event->type == SDL_KEYDOWN)
     {
-    case SDL_KEYDOWN:
-    {
-        if (event->key.state == SDL_PRESSED)
+        if (interactor->actor_selected != NULL)
         {
-            switch (event->key.keysym.sym)
-            {
-            case SDLK_LEFTBRACKET:
-            {
-                if (interactor->actor_selected == NULL)
-                {
-                    interactor->actor_selected = &interactor->scene_target->actor;
-                }
-            } break;
-            case SDLK_RIGHTBRACKET:
-            {
-                if (interactor->actor_selected == NULL)
-                {
-                    interactor->actor_selected = &interactor->scene_target->actor;
-                }
-            } break;
-            case SDLK_z:
-            {
-                if (interactor->actor_selected != NULL)
-                {
-                    const t_palette next_palette = (interactor->actor_selected->material.palette + 1) % Palette_COUNT;
-                    actor_palette_set(interactor->actor_selected, next_palette);
-                }
-            } break;
-            case SDLK_h:
-            {
-                if (interactor->actor_selected != NULL)
-                {
-                    interactor->actor_selected->material.wireframe = !interactor->actor_selected->material.wireframe;
-                }
-            } break;
-            case SDLK_g:
-            {
-                if (interactor->actor_selected != NULL)
-                {
-                    interactor->actor_selected->material.grayscale = !interactor->actor_selected->material.grayscale;
-                }
-            } break;
-            case SDLK_f:
-            {
-                if (interactor->actor_selected != NULL)
-                {
-                    interactor->actor_selected->material.smooth = !interactor->actor_selected->material.smooth;
-                }
-            } break;
-            }
+            if (event->key.keysym.sym == SDLK_z)
+                actor_palette_set(a, (a->material.palette + 1) % Palette_COUNT);
+            else if (event->key.keysym.sym == SDLK_f)
+                a->material.smooth = !a->material.smooth;
+            else if (event->key.keysym.sym == SDLK_g)
+                a->material.grayscale = !a->material.grayscale;
+            else if (event->key.keysym.sym == SDLK_h)
+                a->material.wireframe = !a->material.wireframe;
         }
-    } break;
-    case SDL_QUIT:
-    {
-        // TODO: it doesn't work properly
-        // X or alt+f4 don't work
-        app->is_running = FALSE;
-    } break;
+        else if (event->key.keysym.sym == SDLK_LEFTBRACKET ||
+                 event->key.keysym.sym == SDLK_RIGHTBRACKET)
+            interactor->actor_selected = &interactor->scene_target->actor;
     }
+    else if (event->type == SDL_QUIT)
+        app->is_running = FALSE;
 }
 
 void        app_poll_events(t_app* app, t_scene_interactor* interactor)
@@ -95,7 +56,7 @@ void        app_poll_events(t_app* app, t_scene_interactor* interactor)
             break;
         i = 0;
         while (i < event_count)
-            process_event(&event_buffer[i++]);
+            process_event(&event_buffer[i++], app, interactor);
     }
     if (event_count < 0)
     {
